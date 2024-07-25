c77d00f5c5df24d867100be0ece7f6a7f9e03fa5
Changgeuns <ckdrms8110@gmail.com>
2024-07-26 01:29:42 +0900

    0726_새프로젝트 생성후 복습
    일단 프로젝트생성후 등록만
    자주 등록하지않아서
    등록하는데만 애먹었엇음

---
+++ Ycg_UE_240726/Source/Ycg_UE/Ycg_UE.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Ycg_UE.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Ycg_UE, "Ycg_UE" );
