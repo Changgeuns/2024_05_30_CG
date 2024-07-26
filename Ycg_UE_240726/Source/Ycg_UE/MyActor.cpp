59abb8e55e6ef74ecca326349ae11c680acfd744
Changgeuns <ckdrms8110@gmail.com>
2024-07-26 11:00:45 +0900

    복습_엑터

--- Ycg_UE_240726/Source/Ycg_UE/MyActor.cpp
+++ Ycg_UE_240726/Source/Ycg_UE/MyActor.cpp
@@ -2,6 +2,8 @@
 
 
 #include "MyActor.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "Components/StaticMeshComponent.h"
 
 // Sets default values
 AMyActor::AMyActor()
@@ -9,13 +11,25 @@ AMyActor::AMyActor()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	_sm = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	RootComponent = _sm;
+
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> mesh(TEXT("/Script/Engine.StaticMesh'/Game/StarterContent/Props/SM_Chair.SM_Chair'"));
+
+	if (mesh.Succeeded())
+	{
+		_sm->SetStaticMesh(mesh.Object);
+	}
+
+	UE_LOG(LogTemp, Warning, TEXT("Constructor"));
+
 }
 
 // Called when the game starts or when spawned
 void AMyActor::BeginPlay()
 {
 	Super::BeginPlay();
-	
+	UE_LOG(LogTemp, Error, TEXT("BeginPlay"));
 }
 
 // Called every frame
@@ -23,5 +37,22 @@ void AMyActor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	if (GetAttachParentActor() != nullptr)
+	{
+		FVector parentV = GetAttachParentActor()->GetActorLocation();
+		FVector myV = GetActorLocation();
+		FRotator rot = UKismetMathLibrary::FindLookAtRotation(myV, parentV);
+
+		SetActorRotation(rot);
+	}
+	else
+	{
+		FRotator rot = FRotator(0.0f, 90.0f, 0.0f);
+		AddActorLocalRotation(rot * _rotationSpeed * DeltaTime);
+
+		FVector moveV = FVector(0.0f, _moveSpeed, 0.0f);
+		FVector destPos = GetActorLocation() + moveV;
+		SetActorLocation(destPos);
+	}
 }
 
